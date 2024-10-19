4b92e55f57d2824a6edac8ec9509c2422eccd127
RaafatAbdelkader <raafat.abdelmalek77@gmail.com>
2024-10-19 12:17:51 +0200

    retry update

--- src/test/java/base/PropReader.java
+++ src/test/java/base/PropReader.java
@@ -6,16 +6,22 @@ import java.util.Properties;
 public class PropReader {
     static String  filepath="src/test/resources/prop.properties";
     public static Object getProp(String key){
-        FileInputStream fileInputStream= null;
-        Properties properties= null;
-        try {
-            fileInputStream = new FileInputStream(filepath);
-            properties =new Properties();
-            properties.load(fileInputStream);
-        } catch (Exception e) {
-            e.printStackTrace();
+        //test -Psmoke -Dbrowser=chrome
+        if (System.getProperty(key)!=null){
+            return System.getProperty(key);
+        }else {
+            FileInputStream fileInputStream= null;
+            Properties properties= null;
+            try {
+                fileInputStream = new FileInputStream(filepath);
+                properties =new Properties();
+                properties.load(fileInputStream);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return properties.get(key);
         }
-        return properties.get(key);
+
     }
 
 }
